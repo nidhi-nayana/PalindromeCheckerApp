346db9ddee7b8597ab3483253766c6cf7d3aaa4f
nidhi-nayana <nidhinayana0412@gmail.com>
2026-02-13 10:56:29 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,18 @@
+/**
+        *Application entry point.
+                *
+                        * This is the first method executed by the JVM
+*when the program starts.
+
+        *
+
+        *@param args Command -Line arguments
+                */
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+    System.out.println("Welcome toPalindrome Checker Management System!");
+    System.out.println("Version: latest");
+    System.out.println("System initialised successfully");
+    }
+}
